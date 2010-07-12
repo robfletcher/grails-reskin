2569b272d70bca15066c395fbc8618acfa2c1733
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-07-12 17:58:45 +0100

    a bunch more IE styling fixes

--- web-app/js/html5-support.js
+++ web-app/js/html5-support.js
@@ -1,19 +1,5 @@
 $(document).ready(function() {
 	if (!Modernizr.inputtypes.date) {
-		$("input[type=date]").datepicker({dateFormat: $.datepicker.W3C});
+		$("input[type=date], input.date").datepicker({dateFormat: $.datepicker.W3C});
 	}
-//	if (!Modernizr.inputtypes.range) {
-//		$("input[type=range]").each(function() {
-//			console.log($(this));
-//			var min, max = null;
-//			if ($(this).attr("min") != undefined) {
-//				min = parseInt($(this).attr("min"));
-//			}
-//			if ($(this).attr("max") != undefined) {
-//				max = parseInt($(this).attr("max"));
-//			}
-//			console.log("min", min, "max", max);
-//			$(this).slider({min: min, max: max});
-//		});
-//	}
 });
