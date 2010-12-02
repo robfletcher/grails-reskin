cea18ba970d3c397153f9a2bcb6ccd74d1dc991c
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-12-02 05:19:57 +0000

    removed unused module classes

---
+++ test/functional/grails/plugin/reskin/pages/HomePage.groovy
@@ -0,0 +1,11 @@
+package grails.plugin.reskin.pages
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rob
+ * Date: 02/12/2010
+ * Time: 04:21
+ * To change this template use File | Settings | File Templates.
+ */
+class HomePage {
+}
