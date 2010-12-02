5aaa1a9ed2b0656332ea451aa30ebfee6e71693d
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-12-02 04:14:23 +0000

    removed duplicate cruft from layout

---
+++ test/functional/grails/plugin/reskin/NavigationSpec.groovy
@@ -0,0 +1,11 @@
+package grails.plugin.reskin
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rob
+ * Date: 02/12/2010
+ * Time: 04:12
+ * To change this template use File | Settings | File Templates.
+ */
+class NavigationSpec {
+}
